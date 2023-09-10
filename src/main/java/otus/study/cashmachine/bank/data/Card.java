63241bf519609b37abd7877b0f8931a128523212
VladislavBodikov <krullerr@yandex.ru>
2023-09-10 09:28:51 +0300

    implement TODO tasks and make 80% line coverage

--- src/main/java/otus/study/cashmachine/bank/data/Card.java
+++ src/main/java/otus/study/cashmachine/bank/data/Card.java
@@ -7,6 +7,28 @@ public class Card {
     private String number;
     private String pinCode;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Card card = (Card) o;
+
+        if (id != card.id) return false;
+        if (accountId != null ? !accountId.equals(card.accountId) : card.accountId != null) return false;
+        if (number != null ? !number.equals(card.number) : card.number != null) return false;
+        return pinCode != null ? pinCode.equals(card.pinCode) : card.pinCode == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (accountId != null ? accountId.hashCode() : 0);
+        result = 31 * result + (number != null ? number.hashCode() : 0);
+        result = 31 * result + (pinCode != null ? pinCode.hashCode() : 0);
+        return result;
+    }
+
     public Card(final long id, final String number, final Long accountId, final String pinCode) {
         this.id = id;
         this.number = number;
