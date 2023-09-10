003cbb76ba0cca3c38365db027e0edba6277cbd6
Veronika <ederella@ya.ru>
2023-09-10 08:49:57 +0300

    homework 2

--- src/test/java/otus/study/cashmachine/bank/service/AccountServiceTest.java
+++ src/test/java/otus/study/cashmachine/bank/service/AccountServiceTest.java
@@ -1,10 +1,19 @@
 package otus.study.cashmachine.bank.service;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import otus.study.cashmachine.bank.dao.AccountDao;
+import otus.study.cashmachine.bank.data.Account;
 import otus.study.cashmachine.bank.service.impl.AccountServiceImpl;
 
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 public class AccountServiceTest {
 
@@ -12,29 +21,68 @@ public class AccountServiceTest {
 
     AccountServiceImpl accountServiceImpl;
 
+    Account accountMock;
+
+    final long TEST_ID = 88888888L;
+    final BigDecimal TEST_AMOUNT = new BigDecimal("6600.00");
+
+    @BeforeEach
+    void init(){
+        accountMock = mock(Account.class);
+        when(accountMock.getId()).thenReturn(TEST_ID);
+        when(accountMock.getAmount()).thenReturn(TEST_AMOUNT);
+
+        accountDao = mock(AccountDao.class);
+        accountServiceImpl = new AccountServiceImpl(accountDao);
+    }
     @Test
     void createAccountMock() {
-// @TODO test account creation with mock and ArgumentMatcher
+        when(accountDao.saveAccount(any())).thenReturn(accountMock);
+
+        Account account = accountServiceImpl.createAccount(TEST_AMOUNT);
+        assertEquals(TEST_ID, account.getId());
+        assertEquals(TEST_AMOUNT, account.getAmount());
     }
 
     @Test
     void createAccountCaptor() {
-//  @TODO test account creation with ArgumentCaptor
+        ArgumentCaptor<Account> accountCaptor = ArgumentCaptor.forClass(Account.class);
+        when(accountDao.saveAccount(accountCaptor.capture())).thenReturn(accountMock);
+
+        Account account = accountServiceImpl.createAccount(TEST_AMOUNT);
+        assertEquals(TEST_ID, account.getId());
+        assertEquals(TEST_AMOUNT, account.getAmount());
     }
 
     @Test
-    void addSum() {
+    void putMoney() {
+        when(accountDao.getAccount(anyLong())).thenReturn(new Account(TEST_ID, TEST_AMOUNT));
+        assertEquals(TEST_AMOUNT.add(BigDecimal.TEN), accountServiceImpl.putMoney(TEST_ID, BigDecimal.TEN));
     }
 
     @Test
-    void getSum() {
+    void getMoney() {
+        when(accountDao.getAccount(anyLong())).thenReturn(new Account(TEST_ID, TEST_AMOUNT));
+        assertEquals(new BigDecimal("0.00"), accountServiceImpl.getMoney(TEST_ID, TEST_AMOUNT));
+
+        BigDecimal tooBigAmount = TEST_AMOUNT.add(BigDecimal.ONE);
+        Exception exception =  assertThrows(IllegalArgumentException.class,
+                () -> accountServiceImpl.getMoney(TEST_ID, tooBigAmount));
+        assertEquals(exception.getMessage(), "Not enough money");
     }
 
     @Test
     void getAccount() {
+        when(accountDao.getAccount(anyLong())).thenReturn(accountMock);
+        Account account = accountServiceImpl.getAccount(TEST_ID);
+        assertEquals(TEST_AMOUNT, account.getAmount());
+        assertEquals(TEST_ID, account.getId());
     }
 
     @Test
     void checkBalance() {
+        when(accountDao.getAccount(anyLong())).thenReturn(accountMock);
+        BigDecimal amount = accountServiceImpl.checkBalance(TEST_ID);
+        assertEquals(TEST_AMOUNT, amount);
     }
 }
--- src/test/java/otus/study/cashmachine/machine/service/CashMachineServiceTest.java
+++ src/test/java/otus/study/cashmachine/machine/service/CashMachineServiceTest.java
@@ -5,12 +5,22 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+import otus.study.cashmachine.TestUtil;
 import otus.study.cashmachine.bank.dao.CardsDao;
+import otus.study.cashmachine.bank.data.Card;
 import otus.study.cashmachine.bank.service.AccountService;
 import otus.study.cashmachine.bank.service.impl.CardServiceImpl;
 import otus.study.cashmachine.machine.data.CashMachine;
 import otus.study.cashmachine.machine.data.MoneyBox;
 import otus.study.cashmachine.machine.service.impl.CashMachineServiceImpl;
+
+import java.util.Arrays;
+import java.util.List;
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CashMachineServiceTest {
@@ -30,35 +40,64 @@ class CashMachineServiceTest {
 
     private CashMachineServiceImpl cashMachineService;
 
-    private CashMachine cashMachine = new CashMachine(new MoneyBox());
+    final private CashMachine cashMachine = new CashMachine(new MoneyBox());
+
+    final String TEST_CARD_NUMBER = "1234_5678_9009_8765";
+    final long TEST_ACC_ID = 88888888L;
+    final String TEST_PIN = "0000";
+    final BigDecimal TEST_AMOUNT = new BigDecimal(6600);
 
     @BeforeEach
     void init() {
         cashMachineService = new CashMachineServiceImpl(cardService, accountService, moneyBoxService);
+        when(cardsDao.getCardByNumber(any())).thenReturn(new Card(0, TEST_CARD_NUMBER, TEST_ACC_ID, TestUtil.getHash(TEST_PIN)));
     }
 
 
     @Test
     void getMoney() {
-// @TODO create get money test using spy as mock
+        when(cardService.getMoney(TEST_CARD_NUMBER, TEST_PIN, TEST_AMOUNT)).thenReturn(TEST_AMOUNT);
+        when(moneyBoxService.getMoney(any(),anyInt())).thenReturn(Arrays.asList(1,1,1,1));
+        List<Integer> banknotes = cashMachineService.getMoney(cashMachine, TEST_CARD_NUMBER, TEST_PIN, TEST_AMOUNT);
+        assertEquals(1, banknotes.get(0));
+        assertEquals(1, banknotes.get(1));
+        assertEquals(1, banknotes.get(2));
+        assertEquals(1, banknotes.get(3));
+
     }
 
     @Test
     void putMoney() {
+        when(cardService.getBalance(TEST_CARD_NUMBER, TEST_PIN)).thenReturn(TEST_AMOUNT);
+        when(cardService.putMoney(TEST_CARD_NUMBER, TEST_PIN, TEST_AMOUNT)).thenReturn(TEST_AMOUNT);
+
+        BigDecimal balance = cashMachineService.putMoney(cashMachine, TEST_CARD_NUMBER, TEST_PIN, Arrays.asList(1, 1, 1, 1));
+        assertEquals(TEST_AMOUNT, balance);
     }
 
     @Test
     void checkBalance() {
-
+        when(cardService.getBalance(TEST_CARD_NUMBER, TEST_PIN)).thenReturn(TEST_AMOUNT);
+        assertEquals(TEST_AMOUNT, cashMachineService.checkBalance(cashMachine, TEST_CARD_NUMBER, TEST_PIN));
     }
 
     @Test
     void changePin() {
-// @TODO create change pin test using spy as implementation and ArgumentCaptor and thenReturn
+        ArgumentCaptor<String> cardNumberCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> oldPinCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> newPinCaptor = ArgumentCaptor.forClass(String.class);
+
+        when(cardService.cnangePin(cardNumberCaptor.capture(), oldPinCaptor.capture(), newPinCaptor.capture())).thenReturn(true);
+        assertTrue(cashMachineService.changePin(TEST_CARD_NUMBER, TEST_PIN, "2023"));
+
     }
 
     @Test
     void changePinWithAnswer() {
-// @TODO create change pin test using spy as implementation and mock an thenAnswer
+        when(cardService.cnangePin(TEST_CARD_NUMBER, TEST_PIN, "2023")).thenAnswer(input-> true);
+        when(cardService.cnangePin(TEST_CARD_NUMBER, TEST_PIN, "2024")).thenAnswer(input->false);
+
+        assertTrue(cashMachineService.changePin(TEST_CARD_NUMBER, TEST_PIN, "2023"));
+        assertFalse(cashMachineService.changePin(TEST_CARD_NUMBER, TEST_PIN, "2024"));
     }
 }
