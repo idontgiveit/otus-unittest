701a8a813321dabc3f3fb58bc9fc2df4996142dd
Admin <tornado-77@yandex.ru>
2023-09-14 07:39:44 +0300

    HW2 done

--- src/test/java/otus/study/cashmachine/bank/service/AccountServiceTest.java
+++ src/test/java/otus/study/cashmachine/bank/service/AccountServiceTest.java
@@ -1,9 +1,18 @@
 package otus.study.cashmachine.bank.service;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.*;
 import otus.study.cashmachine.bank.dao.AccountDao;
+import otus.study.cashmachine.bank.data.Account;
+import otus.study.cashmachine.bank.db.Accounts; // убрать
 import otus.study.cashmachine.bank.service.impl.AccountServiceImpl;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import static org.mockito.Mockito.verify;
 
 
 public class AccountServiceTest {
@@ -15,11 +24,33 @@ public class AccountServiceTest {
     @Test
     void createAccountMock() {
 // @TODO test account creation with mock and ArgumentMatcher
+
+        Account accBasic = new Account(15L, new BigDecimal(5000));
+
+        AccountServiceImpl accountServiceImpl = Mockito.mock(AccountServiceImpl.class);
+        Mockito.when(accountServiceImpl.createAccount(ArgumentMatchers.any())).thenReturn(new Account(15L, new BigDecimal(5000)));
+
+        Account acc = accountServiceImpl.createAccount(new BigDecimal(5000));
+
+        Assertions.assertEquals(acc, accBasic);
     }
 
     @Test
     void createAccountCaptor() {
 //  @TODO test account creation with ArgumentCaptor
+        Account accBasic = new Account(15L, new BigDecimal(5000));
+
+        ArgumentCaptor<BigDecimal> captor = ArgumentCaptor.forClass(BigDecimal.class);
+        AccountServiceImpl accountServiceImpl = Mockito.mock(AccountServiceImpl.class);
+
+        Mockito.when(accountServiceImpl.createAccount(ArgumentMatchers.any())).thenReturn(new Account(15L, new BigDecimal(5000)));
+
+        Account acc = accountServiceImpl.createAccount(new BigDecimal(5000));
+
+        verify(accountServiceImpl).createAccount(captor.capture());
+
+        Assertions.assertEquals(new BigDecimal(5000), captor.getValue());
+        Assertions.assertEquals(acc, accBasic);
     }
 
     @Test
