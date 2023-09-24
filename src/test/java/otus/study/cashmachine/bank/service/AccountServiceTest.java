8260edee9108771425eaab3be86a15e23e645bac
Pulus <kaurovpavel@gmail.com>
2023-09-24 17:03:26 +0300

    homework to check

--- src/test/java/otus/study/cashmachine/bank/service/AccountServiceTest.java
+++ src/test/java/otus/study/cashmachine/bank/service/AccountServiceTest.java
@@ -1,25 +1,55 @@
 package otus.study.cashmachine.bank.service;
 
-
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 import otus.study.cashmachine.bank.dao.AccountDao;
-import otus.study.cashmachine.bank.service.impl.AccountServiceImpl;
+import otus.study.cashmachine.bank.data.Account;
 
+import java.math.BigDecimal;
+import java.util.Objects;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class AccountServiceTest {
-
-    AccountDao accountDao;
-
-    AccountServiceImpl accountServiceImpl;
+    @Mock
+    private AccountDao accountDao;
+    @Mock
+    private Account account;
 
     @Test
     void createAccountMock() {
-// @TODO test account creation with mock and ArgumentMatcher
+        //@TODO test account creation with mock and ArgumentMatcher
+        when(accountDao.saveAccount(argThat(argument ->
+                Objects.equals(argument, new Account(1L, new BigDecimal(1000))))))
+                .thenReturn(new Account(1L, new BigDecimal(1000)));
+
+        Account newAccount = accountDao.saveAccount(new Account(1L,new BigDecimal(1000)));
+
+        assertNotEquals(0, newAccount.getId());
+        assertEquals(1L, newAccount.getId());
+        assertEquals(new BigDecimal(1000), newAccount.getAmount());
     }
 
     @Test
     void createAccountCaptor() {
-//  @TODO test account creation with ArgumentCaptor
+        // @TODO test account creation with ArgumentCaptor
+        //Создание тестовой учетной записи с помощью ArgumentCaptor
+        ArgumentCaptor<Account> accountCaptor = ArgumentCaptor.forClass(Account.class);
+
+        when(accountDao.saveAccount(accountCaptor.capture())).thenReturn(
+                new Account(1L,new BigDecimal(2000)));
+
+        Account newAccount = accountDao.saveAccount(account);
+
+        assertNotEquals(0, newAccount.getId());
+        assertEquals(1L, newAccount.getId());
+        assertEquals(new BigDecimal(2000), newAccount.getAmount());
     }
 
     @Test
--- src/test/java/otus/study/cashmachine/machine/service/CashMachineServiceTest.java
+++ src/test/java/otus/study/cashmachine/machine/service/CashMachineServiceTest.java
@@ -12,9 +12,16 @@ import otus.study.cashmachine.machine.data.CashMachine;
 import otus.study.cashmachine.machine.data.MoneyBox;
 import otus.study.cashmachine.machine.service.impl.CashMachineServiceImpl;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
 class CashMachineServiceTest {
-
     @Spy
     @InjectMocks
     private CardServiceImpl cardService;
@@ -41,6 +48,16 @@ class CashMachineServiceTest {
     @Test
     void getMoney() {
 // @TODO create get money test using spy as mock
+        //ArgumentCaptor<Account> accountCaptor = ArgumentCaptor.forClass(Account.class);
+        //создайте тест на получение денег, используя spy в качестве мока
+        doReturn(new BigDecimal(1000)).when(cardService).getMoney(any(), any(), any());
+        when(moneyBoxService.getMoney(cashMachine.getMoneyBox(), 1000)).thenReturn(List.of(1000));
+
+
+        List<Integer> money = cashMachineService.getMoney(cashMachine, "1111", "0000", new BigDecimal(1000));
+        assertNotNull(money);
+        assertEquals(1, money.size());
+        assertEquals(1000, money.get(0));
     }
 
     @Test
@@ -55,10 +72,34 @@ class CashMachineServiceTest {
     @Test
     void changePin() {
 // @TODO create change pin test using spy as implementation and ArgumentCaptor and thenReturn
+        //создайте тест на изменение pin-кода, используя spy в качестве реализации и ArgumentCaptor и thenReturn
+
+        ArgumentCaptor<String> cardNumCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> oldPinCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> newPinCaptor = ArgumentCaptor.forClass(String.class);
+
+        doReturn(true).when(cardService).cnangePin(any(), any(), any());
+
+        cashMachineService.changePin("11111", "0000", "1234");
+        verify(cardService, times(1))
+                .cnangePin(cardNumCaptor.capture(), oldPinCaptor.capture(), newPinCaptor.capture());
+        assertEquals("11111", cardNumCaptor.getValue());
+        assertEquals("0000", oldPinCaptor.getValue());
+        assertEquals("1234", newPinCaptor.getValue());
     }
 
     @Test
     void changePinWithAnswer() {
 // @TODO create change pin test using spy as implementation and mock an thenAnswer
+        //создайте тест на изменение pin-кода, используя spy в качестве реализации, и смоделируйте ответ thenAnswer
+        doAnswer(invocation -> {
+            Object arg0 = invocation.getArgument(0);
+            Object arg1 = invocation.getArgument(1);
+
+            assertEquals("11111", arg0);
+            assertEquals("0000", arg1);
+            return null;
+        }).when(cardService).cnangePin(any(),any(),any());
+        cardService.cnangePin("11111","0000","11111");
     }
 }
